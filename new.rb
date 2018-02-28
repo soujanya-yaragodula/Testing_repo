179582157bbf2cd15ef0ffb0ab7478688a83bcda
Soujanya Yaragodula <soujanya.yaragodula@i28822-is4.octanner.com>
2018-02-28 11:25:04 -0700

    finding the number of characters in your name

--- new.rb
+++ new.rb
@@ -5,5 +5,5 @@ puts "whats ur middle name?"
 mname = gets.chomp
 puts "whats ur last name?"
 lname = gets.chomp
-length = fname.lenght+mname.length+lname.length
+length = fname.length+mname.length+lname.length
 puts "The number of characters in ur name" + fname +" "+mname+" "+lname+" are" +length.to_s
