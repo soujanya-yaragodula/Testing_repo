d2912b8cd42016bd125c1c25616125adad547638
Soujanya Yaragodula <soujanya.yaragodula@i28822-is4.octanner.com>
2018-02-28 11:18:54 -0700

    finding the number of characters in your name

--- new.rb
+++ new.rb
@@ -1,1 +1,9 @@
 #new ruby file...
+puts "whats ur first name?"
+fname = gets.chomp
+puts "whats ur middle name?"
+mname = gets.chomp
+puts "whats ur last name?"
+lname = gets.chomp
+length = fname.lenght+mname.length+lname.length
+puts "The number of characters in ur name" + fname +" "+mname+" "+lname+" are" +length.to_s
