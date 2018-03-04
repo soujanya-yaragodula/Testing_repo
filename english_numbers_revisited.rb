cfa259fac16e4ca688c8a943afb926836f82bd49
Soujanya Yaragodula <soujanya.yaragodula@i28822-is4.octanner.com>
2018-03-04 11:57:24 -0700

    tried out a few

--- english_numbers_revisited.rb
+++ english_numbers_revisited.rb
@@ -14,50 +14,48 @@ tens_place = ['ten',       'twenty',   'thirty',
 teenagers  = ['eleven',    'twelve',   'thirteen',
               'fourteen',  'fifteen',  'sixteen',
               'seventeen', 'eighteen', 'nineteen']
-left = number
-write = left/100
-left = left - write*100
-if write > 0
-  hundreds = english_number write
-  num_string = num_string + hundreds + ' hundred'
-  if left > 0
-    num_string = num_string + ' '
-  end
-end
-
-write = left/10
+            next_places = [['hundred',            2],
+                          ['thousand',           3],
+                          ['million',            6],
+                          ['billion',            9]]
+              left = number
+              while next_places.length > 0
+                num_pair = next_places.pop
+                num_name = num_pair[0]
+                num_base = 10 ** num_pair[1]
+                write = left/num_base
+                left = left - write*num_base
+              if write > 0
+                  prefix = english_number write
+                  num_string = num_string + prefix + ' ' + num_name
+              if left > 0
+                 num_string = num_string + ' '
+              end
+            end
+          end
+          write = left/10
 left = left - write*10
 if write > 0
-  if ((write == 1) and (left > 0))
-    num_string = num_string + teenagers[left-1]
-    left = 0
-  else
-        num_string = num_string + tens_place[write-1]
+if ((write == 1) and (left > 0))
+
+num_string = num_string + teenagers[left-1]
+
+left = 0
+else
+      num_string = num_string + tens_place[write-1]
 
 end
 if left > 0
-    num_string = num_string + '-'
-  end
-end
-  write = left
+
+num_string = num_string + '-'
+end end
+write = left
+ left = 0
 if write > 0
-  num_string = num_string + ones_place[write-1]
+num_string = num_string + ones_place[write-1]
 end
-  num_string
+num_string
 end
-puts ("enter a number:")
+puts ("Enter a number")
 num = gets.to_i
 puts (english_number(num))
-
-while true
-  puts ("do you want to enter another number:(y/n)")
-  ans = gets.chomp
-  if ans == 'y' || ans == 'Y' || ans =='yes' || ans == 'Yes' || ans == 'YES'
-    puts("enter the number")
-    num = gets.to_i
-    puts(english_number(num))
-  else
-    puts("bye")
-    break
-  end
-end
